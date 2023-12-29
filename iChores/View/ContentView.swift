63f887b9225fd666fb645f2aeed6854250b0b73b
CarolaneLFBV <107761189+CarolaneLFBV@users.noreply.github.com>
2023-12-29 14:41:01 +0100

    fix: commit 2/2

--- iChores/View/ContentView.swift
+++ iChores/View/ContentView.swift
@@ -9,57 +9,30 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @State private var isShowing = false
-    @FetchRequest(sortDescriptors: []) var users: FetchedResults<User>
-    @FetchRequest(sortDescriptors: []) var rooms: FetchedResults<Room>
-    @FetchRequest(sortDescriptors: []) var tasks: FetchedResults<Task>
-    
     var body: some View {
-        NavigationStack {
-            Section {
-                List {
-                    ForEach(users) { user in
-                        NavigationLink(destination: UserDetailView(user: user)) {
-                            Text(user.wrappedUserName)
-                        }
-                    }
-                    .onDelete(perform: { indexSet in
-                        let selectedUser = indexSet.map { users[$0] }
-                        deleteObject(Set(selectedUser), context: moc)
-                    })
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
                 }
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        isShowing.toggle()
-                    } label: {
-                        Label("Add user", systemImage: "plus")
-                    }
+            
+            RoomsListView()
+                .tabItem {
+                    Label("Rooms", systemImage: "square.split.bottomrightquarter")
                 }
-            }
-            .sheet(isPresented: $isShowing) {
-                AddUserView()
-            }
+            
+            UsersListView()
+                .tabItem {
+                    Label("Users", systemImage: "person.2")
+                }
+            
+            UserProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
         }
     }
 }
-
-extension ContentView {
-    func deleteObject<T: NSManagedObject>(_ objects: Set<T>, context: NSManagedObjectContext) {
-        for object in objects {
-            context.delete(object)
-        }
-
-        do {
-            try context.save()
-        } catch {
-            print("Error while deleting data \(error.localizedDescription)")
-        }
-    }
-}
-
 #Preview {
     ContentView()
 }
--- iChores/View/UserView/UsersListView.swift
+++ iChores/View/UserView/UsersListView.swift
@@ -6,12 +6,58 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct UsersListView: View {
+    @Environment(\.managedObjectContext) var moc
+    @State private var isShowing = false
+    @FetchRequest(sortDescriptors: []) var users: FetchedResults<User>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Section {
+                List {
+                    ForEach(users, id: \.self) { user in
+                        NavigationLink(destination: UserDetailView(user: user)) {
+                            Text(user.wrappedUserName)
+                        }
+                    }
+                    .onDelete(perform: { indexSet in
+                        let selectedUser = indexSet.map { users[$0] }
+                        deleteObject(Set(selectedUser), context: moc)
+                    })
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        isShowing.toggle()
+                    } label: {
+                        Label("Add user", systemImage: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowing) {
+                AddUserView()
+            }
+        }
     }
 }
+
+extension UsersListView {
+    func deleteObject<T: NSManagedObject>(_ objects: Set<T>, context: NSManagedObjectContext) {
+        for object in objects {
+            context.delete(object)
+        }
+
+        do {
+            try context.save()
+        } catch {
+            print("Error while deleting data \(error.localizedDescription)")
+        }
+    }
+}
+
 
 #Preview {
     UsersListView()
