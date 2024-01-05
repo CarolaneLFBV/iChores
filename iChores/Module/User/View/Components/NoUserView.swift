b3d64d9c0eb7241ff37303083707959925d519e3
CarolaneLFBV <107761189+CarolaneLFBV@users.noreply.github.com>
2024-01-05 20:52:32 +0100

    feat: no user view

---
+++ iChores/Module/User/View/Components/NoUserView.swift
@@ -0,0 +1,18 @@
+//
+//  NoUserView.swift
+//  iChores
+//
+//  Created by Carolane Lefebvre on 05/01/2024.
+//
+
+import SwiftUI
+
+struct NoUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoUserView()
+}
